1844260beb12fe90f1634cf761cb1c55a0b5ac22
noViceMin <admin@isamin.kr>
2024-03-09 01:21:16 +0900

    feat: analytics

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -16,6 +16,8 @@ export const RouterHead = component$(() => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" href="/favicon.ico" />
 
+      <script defer data-domain="surfing.dimigo.in" src="https://analytics.2w.vc/js/script.js"></script>
+
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
       ))}
